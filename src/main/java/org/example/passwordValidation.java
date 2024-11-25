e54ec067af224ea71582386c84ae0c236559886e
Torben Jost <torbenjost@proton.me>
2024-11-25 11:33:35 +0100

    define method names

---
+++ src/main/java/org/example/passwordValidation.java
@@ -0,0 +1,21 @@
+package org.example;
+
+public class passwordValidation {
+
+    // method to check the length of the password is at least 8 characters: pwLength_min8
+
+
+
+    // method to check if the password contains digits: pwContains_digits
+
+
+
+    // method to check if the password contains digits: pwContains_lowerCaseUpperCase
+
+
+
+    // method to check if the password is commonlyUser: pwIs_commonlyUsed
+
+
+    
+}
